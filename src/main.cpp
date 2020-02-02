63fff3d5a9310f15b5e425f8de407967165dbf87
iBessonob <bessonov3145@gmail.com>
2020-02-02 00:04:35 -0800

    Added emitter port A1. Unit testing to be fixed.

--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,30 @@
 */
 #include <Arduino.h>
 #define receiver A0 
+#define emitter A1
 
 void setup() {
-  pinMode(receiver,INPUT); //pass in var receiver a0 into INPUT keyword is in Arduino library
-  Serial.begin(9600); //start serial monitor
-
+  pinMode(receiver,INPUT); //pass in var receiver A0 
+  pinMode(emitter,OUTPUT); //pass in var emitter A1
+  Serial.begin(9600); //starts serial monitor
   //unsigned long baud 9600
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  /*digitalWrite(receiver,LOW); //write signal to LED port off light
-  delay(1000); //ms
-  digitalWrite(receiver,HIGH); //write signal to LED port bright*/
+  digitalWrite(emitter,HIGH);
+  delay(1000);
   Serial.println(analogRead(receiver));
-  delay(1000); //ms
-  //Serial.println("loop completed");
+  digitalWrite(emitter,LOW);
+  delay(1000); 
 }
 
 /*
-* Once you are done with code, clicke the check button
+* Once you are done with code, click the check button
 * to build the cpp project,
 * then to the right of the check butotn, click the right button
 * to upload the code to the teensy 3.2
 *
-* to view the serial monitor output for the teensy, lick on the plug
+* to view the serial monitor output for the teensy, click on the plug icon+6
 *
 * Movement Class
 * Sensor Class
--- test/test_main.cpp
+++ test/test_main.cpp
@@ -10,45 +10,22 @@
 // }
 
 void test_receiver_builtin_pin_number(void) {
-    TEST_ASSERT_EQUAL(A0, );
-    //TEST_ASSERT_
+    TEST_ASSERT_EQUAL(A0, PIN_A0);
+    
 }
 
-void test_receiver_state_high(void) {
-    analogRead(receiver);
-    TEST_ASSERT_EQUAL(HIGH, analogRead(receiver));
+void test_receiver_state_high(void){
+    //int otpt = analogRead(receiver);
+    TEST_ASSERT_GREATER_THAN(true,50>40);
 }
 
-void test_receiver_state_low(void) {
-    analogRead(receiver);
-    TEST_ASSERT_EQUAL(LOW, analogRead(receiver);
+int main(int argc, char **argv)
+{
+    UNITY_BEGIN();
+
+    //RUN_TEST(test_receiver_builtin_pin_number());
+
+    return UNITY_END();
 }
 
-void setup() {
-    // NOTE!!! Wait for >2 secs
-    // if board doesn't support software reset via Serial.DTR/RTS
-    delay(2000);
-
-    UNITY_BEGIN();    // IMPORTANT LINE!
-    RUN_TEST(test_led_builtin_pin_number);
-
-    pinMode(LED_BUILTIN, OUTPUT);
-}
-
-uint8_t i = 0;
-uint8_t max_blinks = 5;
-
-void loop() {
-    if (i < max_blinks)
-    {
-        RUN_TEST(test_led_state_high);
-        delay(500);
-        RUN_TEST(test_led_state_low);
-        delay(500);
-        i++;
-    }
-    else if (i == max_blinks) {
-      UNITY_END(); // stop unit testing
-    }
-}
 
